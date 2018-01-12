05c34b9fbf7f3803e8ebaf5804a093beacb9bde1
Brandon Cranmore <brandoncranmore@gmail.com>
2018-01-12 09:56:50 -0800

    initial commit

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
+import jQuery from 'jquery';
 import Popper from 'popper.js';
 
 window.$ = $;
+window.jQuery = $;
 
 import Bootstrap from 'bootstrap';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
